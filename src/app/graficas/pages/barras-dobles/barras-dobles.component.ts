7788bb56c06280d7620605332d0223da5a91e292
Miguel <miguel.fuentes@bosonit.com>
2022-06-29 17:09:46 +0200

    start proyerct

---
+++ src/app/graficas/pages/barras-dobles/barras-dobles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barras-dobles',
+  templateUrl: './barras-dobles.component.html',
+  styleUrls: []
+})
+export class BarrasDoblesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
